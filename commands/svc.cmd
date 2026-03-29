9b47e31c8bca0f96eee9f5e45125e6434bc759a1
Stanislav Ilnytskyi <stailx1@gmail.com>
2026-03-29 11:11:39 +0200

    Clarify DoH PKI validation comments

--- commands/svc.cmd
+++ commands/svc.cmd
@@ -128,6 +128,9 @@ if [[ "${WARDEN_PARAMS[0]}" == "up" ]]; then
         cp "${WARDEN_SSL_DIR}/rootca/crl/ca.crl.pem" "${WARDEN_HOME_DIR}/etc/pki-public/ca.crl.pem"
     fi
 
+    # Keep normal HTTP->HTTPS redirects in dynamic config so `/.warden/pki/`
+    # can remain opt-in plain HTTP for DoH clients that require working
+    # CRL/AIA retrieval during TLS validation.
     cat >> "${WARDEN_HOME_DIR}/etc/traefik/dynamic.yml" <<-'EOT'
 		http:
 		  routers:
